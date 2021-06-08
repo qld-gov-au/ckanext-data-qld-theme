ba547485c41cc264f12422e17d4d614af15bad39
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-06-08 10:01:37 +1000

    [QOL-8050] go back to using Ahoy

--- .docker/scripts/create-test-data.sh
+++ .docker/scripts/create-test-data.sh
@@ -7,16 +7,14 @@ set -e
 
 CKAN_ACTION_URL=http://ckan:3000/api/action
 
-cd $SRC_DIR/ckan
-
 # We know the "admin" sysadmin account exists, so we'll use her API KEY to create further data
 API_KEY=$(paster --plugin=ckan user admin -c ${CKAN_INI} | tr -d '\n' | sed -r 's/^(.*)apikey=(\S*)(.*)/\2/')
 
 # Creating test data hierarchy which creates organisations assigend to datasets
-paster create-test-data hierarchy -c ${CKAN_INI}
+paster --plugin=ckan create-test-data hierarchy -c ${CKAN_INI}
 
 # Creating basic test data which has datasets with resources
-paster create-test-data -c ${CKAN_INI}
+paster --plugin=ckan create-test-data -c ${CKAN_INI}
 
 echo "Updating annakarenina to use department-of-health Organisation:"
 package_owner_org_update=$( \
--- .docker/scripts/init.sh
+++ .docker/scripts/init.sh
@@ -8,9 +8,8 @@ CKAN_USER_NAME="${CKAN_USER_NAME:-admin}"
 CKAN_USER_PASSWORD="${CKAN_USER_PASSWORD:-Password123!}"
 CKAN_USER_EMAIL="${CKAN_USER_EMAIL:-admin@localhost}"
 
-cd $SRC_DIR/ckan \
-  && paster db clean -c $CKAN_INI \
-  && paster db init -c $CKAN_INI \
+paster --plugin=ckan db clean -c $CKAN_INI \
+  && paster --plugin=ckan db init -c $CKAN_INI \
   && paster --plugin=ckan user add "${CKAN_USER_NAME}" email="${CKAN_USER_EMAIL}" password="${CKAN_USER_PASSWORD}" -c $CKAN_INI \
   && paster --plugin=ckan sysadmin add "${CKAN_USER_NAME}" -c $CKAN_INI
 
